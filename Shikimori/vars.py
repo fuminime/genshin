9143de55f0b53a3674c50424ed4cfcde2b52a922
YUN神 <110736685+Yunxvoid@users.noreply.github.com>
2022-12-03 05:08:46 -0700

    Update vars.py

--- Shikimori/vars.py
+++ Shikimori/vars.py
@@ -7,7 +7,7 @@ if ENV:
     TOKEN = os.environ.get("BOT_TOKEN", None)
 
     try:
-        OWNER_ID = int(os.environ.get("OWNER_ID", "None"))
+        OWNER_ID = int(os.environ.get("OWNER_ID", "5001899507"))
     except ValueError:
         raise Exception("Your OWNER_ID env variable is not a valid integer.")
      
