3b2a3301b3410bfeb394641a31f79979ba2c5c5d
YUN神 <110736685+Yunxvoid@users.noreply.github.com>
2022-12-03 08:48:31 -0700

    Update __init__.py

--- Shikimori/__init__.py
+++ Shikimori/__init__.py
@@ -46,8 +46,8 @@ logging.getLogger("pyrogram").setLevel(logging.INFO)
 logging.getLogger('ptbcontrib.postgres_persistence.postgrespersistence').setLevel(logging.WARNING)
 
 LOGGER = logging.getLogger('[Shikimori]')
-LOGGER.info("Shikimori is starting. | Built by SOME1HING. | Licensed under GPLv3.")
-LOGGER.info("Handled by: github.com/SOME-1HING (t.me/SOME1HING)")
+LOGGER.info("Shikimori is starting. | Built by Yun. | Licensed under GPLv3.")
+LOGGER.info("Handled by: github.com/yunxvoid (t.me/Mr_nack_nack)")
 
 # if version < 3.6, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
@@ -75,7 +75,7 @@ print("[INFO]: PYROGRAM CLIENT INITIALIZED")
 # PTB Client
 print("[INFO]: INITIALIZING PTB CLIENT")
 defaults = tg.Defaults(run_async=True)
-updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
+updater = tg.Updater("bot token", workers=WORKERS, use_context=True)
 dispatcher = updater.dispatcher
 print("[INFO]: PTB CLIENT INITIALIZED")
 
