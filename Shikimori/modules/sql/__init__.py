8b0467c003aa063d9c4b30abc8badf0be3b31a4b
Abhinav <113854226+DARK-KING-2304@users.noreply.github.com>
2022-12-04 19:07:30 +0530

    Update __init__.py

--- Shikimori/modules/sql/__init__.py
+++ Shikimori/modules/sql/__init__.py
@@ -30,11 +30,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 
-def start() -> scoped_session:
-    engine = create_engine(DB_URL, client_encoding="utf8")
-    BASE.metadata.bind = engine
-    BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=True))
+if DB_URI and DB_URI.startswith("postgres://"): 
+     DB_URI = DB_URI.replace("postgres://", "postgresql://", 1) 
+  
+def start() -> scoped_session: 
+     engine = create_engine(DB_URI, client_encoding="utf8") 
+     log.info("[PostgreSQL] Connecting to database......")
 
 
 BASE = declarative_base()
